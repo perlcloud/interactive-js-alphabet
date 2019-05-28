c2f5dfdbe5a5305eb075661f03d4e602ead64945
perlcloud <cloud.perl@gmail.com>
2019-05-28 01:42:58 -0400

    Added simulatedKeyPress() on mouse click

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,8 @@
+let soundEffectCount = 1
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-let effectCount = 1
 
 function playEffect() {
     const effects = [
@@ -13,11 +13,11 @@ function playEffect() {
         "5.mp3",
     ];
 
-    let audio = new Audio('media/' + effectCount + '.mp3');
-    if (effectCount == effects.length) {
-        effectCount = 1;
+    let audio = new Audio('media/' + soundEffectCount + '.mp3');
+    if (soundEffectCount == effects.length) {
+        soundEffectCount = 1;
     } else {
-        effectCount += 1
+        soundEffectCount += 1
     }
     audio.play();
 }
@@ -30,25 +30,26 @@ async function playLetter(char) {
 }
 
 function displayChar(e) {
-    try {
+    keyPressed = e.key.toLowerCase()
+    if (typeof (char[keyPressed]) !== "undefined") {
         // Get and display text
-        display = char.display(e.key);
+        display = char.display(keyPressed);
         selectedLetter.textContent = display;
         // Select and display an icon
-        iconObj = char.icon(e.key)
+        iconObj = char.icon(keyPressed)
         icon = iconObj["icon"];
         iconSrc = 'media/' + icon
         iconName = iconObj["iconName"];
         iconName.textContent = iconName;
         document.getElementById('icon').src = iconSrc;
-        document.getElementById('outerWrap').className = e.key
+        document.getElementById('outerWrap').className = keyPressed
         // Play audio
         playEffect();
-        char.audio(e.key); b
-    } catch (err) { }
+        char.audio(keyPressed);
+    }
 }
 
-var char = {
+let char = {
     display: function (c) {
         returnStr = c.toUpperCase() + c.toLowerCase()
         return returnStr;
@@ -69,160 +70,42 @@ var char = {
         await sleep(400);
         audio.play();
     },
-    a: {
-        icons: {
-            apple: "apple.svg",
-            airplane: "airplane.svg",
-        }
-    },
-    b: {
-        icons: {
-            bus: "bus.svg",
-            bug: "bug.svg",
-        }
-    },
-    c: {
-        icons: {
-            cat: "cat.svg",
-            car: "car.svg",
-        }
-    },
-    d: {
-        icons: {
-            dog: "dog.svg",
-            duck: "duck.svg",
-        }
-    },
-    e: {
-        icons: {
-            egg: "egg.svg",
-            elephant: "elephant.svg"
-        }
-    },
-    f: {
-        icons: {
-            fish: "fish.svg",
-            flower: "flower.svg",
-        }
-    },
-    g: {
-        icons: {
-            giraffe: "giraffe.svg",
-            gift: "gift.svg",
-        }
-    },
-    h: {
-        icons: {
-            hat: "hat.svg",
-            heart: "heart.svg",
-        }
-    },
-    i: {
-        icons: {
-            "ice cream": "ice cream.svg",
-            island: "island.svg",
-        }
-    },
-    j: {
-        icons: {
-            jewel: "jewel.svg",
-            jellyfish: "jellyfish.svg",
-        }
-    },
-    k: {
-        icons: {
-            kite: "kite.svg",
-            keyboard: "keyboard.svg",
-        }
-    },
-    l: {
-        icons: {
-            lemon: "lemon.svg",
-            light: "light.svg",
-        }
-    },
-    m: {
-        icons: {
-            monkey: "monkey.svg",
-            mouse: "mouse.svg",
-        }
-    },
-    n: {
-        icons: {
-            net: "net.svg",
-            ninja: "ninja.svg",
-        }
-    },
-    o: {
-        icons: {
-            onion: "onion.svg",
-            owl: "owl.svg",
-            orange: "orange.svg"
-        }
-    },
-    p: {
-        icons: {
-            pen: "pen.svg",
-            police: "police.svg",
-            princess: "princess.svg",
-        }
-    },
-    q: {
-        icons: {
-            question: "question.svg",
-        }
-    },
-    r: {
-        icons: {
-            ring: "ring.svg",
-            rope: "rope.svg",
-        }
-    },
-    s: {
-        icons: {
-            sock: "sock.svg",
-            star: "star.svg",
-        }
-    },
-    t: {
-        icons: {
-            teeth: "teeth.svg",
-            truck: "truck.svg",
-        }
-    },
-    u: {
-        icons: {
-            umbrella: "umbrella.svg",
-        }
-    },
-    v: {
-        icons: {
-            violin: "violin.svg",
-        }
-    },
-    w: {
-        icons: {
-            watermelon: "watermelon.svg",
-            wand: "wand.svg",
-        }
-    },
-    x: {
-        icons: {
-            xylophone: "xylophone.svg",
-        }
-    },
-    y: {
-        icons: {
-            yacht: "yacht.svg",
-        }
-    },
-    z: {
-        icons: {
-            zebra: "zebra.svg",
-        }
-    },
+    a: { icons: { apple: "apple.svg", airplane: "airplane.svg", } },
+    b: { icons: { bus: "bus.svg", bug: "bug.svg", } },
+    c: { icons: { cat: "cat.svg", car: "car.svg", } },
+    d: { icons: { dog: "dog.svg", duck: "duck.svg", } },
+    e: { icons: { egg: "egg.svg", elephant: "elephant.svg" } },
+    f: { icons: { fish: "fish.svg", flower: "flower.svg", } },
+    g: { icons: { giraffe: "giraffe.svg", gift: "gift.svg", } },
+    h: { icons: { hat: "hat.svg", heart: "heart.svg", } },
+    i: { icons: { "ice cream": "ice cream.svg", island: "island.svg", } },
+    j: { icons: { jewel: "jewel.svg", jellyfish: "jellyfish.svg", } },
+    k: { icons: { kite: "kite.svg", keyboard: "keyboard.svg", } },
+    l: { icons: { lemon: "lemon.svg", light: "light.svg", } },
+    m: { icons: { monkey: "monkey.svg", mouse: "mouse.svg", } },
+    n: { icons: { net: "net.svg", ninja: "ninja.svg", } },
+    o: { icons: { onion: "onion.svg", owl: "owl.svg", orange: "orange.svg" } },
+    p: { icons: { pen: "pen.svg", police: "police.svg", princess: "princess.svg", } },
+    q: { icons: { question: "question.svg", } },
+    r: { icons: { ring: "ring.svg", rope: "rope.svg", } },
+    s: { icons: { sock: "sock.svg", star: "star.svg", } },
+    t: { icons: { teeth: "teeth.svg", truck: "truck.svg", } },
+    u: { icons: { umbrella: "umbrella.svg", } },
+    v: { icons: { violin: "violin.svg", } },
+    w: { icons: { watermelon: "watermelon.svg", wand: "wand.svg", } },
+    x: { icons: { xylophone: "xylophone.svg", } },
+    y: { icons: { yacht: "yacht.svg", } },
+    z: { icons: { zebra: "zebra.svg", } },
 }
 
-document.addEventListener('keydown', displayChar);
+function simulateKeyPress() {
+    const simulatedKeys = "abcdefghijklmnopqrstuvwxyz";
+    let simulatedEvent = {
+        key: simulatedKeys[Math.floor(Math.random() * simulatedKeys.length)]
+    };
+    displayChar(simulatedEvent)
+}
+
+document.addEventListener('keyup', displayChar);
 
 
